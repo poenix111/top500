22e9227d5613031cb253dbed6db0b7d6ec1956ba
Edmundo Gonzalez <mundo2599@outlook.com>
2020-01-22 18:29:42 -0600

    Se crea base de datos y llena con scrap de top500

--- database.py
+++ database.py
@@ -1,27 +1,64 @@
 import mysql.connector
+import sys
 
 
 class DbTop500:
     def __init__(self):
+        self.conectado = False
+        self.db_name = 'top500'
+
+    def connect(self, database=True):
         self.db = mysql.connector.connect(
             host="localhost",
             user="root",
-            passwd=""
+            passwd="",
+            database=self.db_name if(database) else None
         )
+        self.conectado = True
 
-    """Crea base de datos y borra si existia una anterior si no se le indica lo contrario
-    Retorna True si todo sale bien y False si ocurre un error"""
-    def create_database(self, drop_if_exists=True) -> bool:
-        try:
-            cursor = self.db.cursor()
-            if(drop_if_exists):
-                cursor.execute("DROP DATABASE IF EXISTS top500")
-            cursor.execute("CREATE DATABASE top500")
-            return True
-        except:
-            return False
-        
+    def disconnect(self):
+        self.db.close()
+        self.conectado = False
+
+    def create_database(self, drop_if_exists=True):
+        self.connect(False)
+
+        cursor = self.db.cursor()
+        if(drop_if_exists):
+            cursor.execute("DROP DATABASE IF EXISTS %s" % self.db_name)
+        cursor.execute("CREATE DATABASE %s" % self.db_name)
+        cursor.execute('''
+            CREATE TABLE %s.registros (
+                id INT AUTO_INCREMENT PRIMARY KEY, 
+                rank VARCHAR(255), 
+                site VARCHAR(255), 
+                pais VARCHAR(255), 
+                system VARCHAR(255), 
+                cores INT, 
+                rmax DOUBLE(11,2), 
+                rpeak DOUBLE(11,2), 
+                power INT, 
+                año INT, 
+                mes INT
+            )''' % self.db_name)
+
+        self.disconnect()
+
+    def insert(self, data: {}):
+        if(not self.conectado):
+            self.connect()
+
+        cursor = self.db.cursor()
+        print(data.values())
+        sql = "INSERT INTO registros VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (None, ) + tuple(data.values())
+        print(values)
+
+        cursor.execute(sql, values)
+        self.db.commit()
+        cursor.close()
+
 
 if __name__ == '__main__':
     db = DbTop500()
-    print(db.create_database(False))
+    db.create_database()
--- top500.py
+++ top500.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from database import DbTop500
 import requests
 
 
@@ -6,7 +7,11 @@ class ScrapTop500:
     def __init__(self):
         self.url = "https://www.top500.org/lists/top500"
         self.urlBase = "https://www.top500.org"
-        self.tableHeads = ["Rank", "Site", "System", "Cores", "Rmax (TFlop/s)", "Power(kW)"]
+        self.tableHeads = ["Rank", "Site", "System",
+                           "Cores", "Rmax (TFlop/s)", "Power(kW)"]
+
+        self.db = DbTop500()
+        self.db.create_database()
 
     def getTop(self):
         response = requests.get(self.url)
@@ -15,7 +20,7 @@ class ScrapTop500:
         links = squares.find_all('a')
         listaComparativas = []
         for ref in links:
-            save = str(ref['href']).replace('lists','list')
+            save = str(ref['href']).replace('lists', 'list')
             listaComparativas.append(save)
 
         # print(listaComparativas)
@@ -25,7 +30,7 @@ class ScrapTop500:
         pages = [1, 2, 3, 4, 5]
         listaComparativas = self.getTop()
         for p in listaComparativas:
-            print (p)
+            print(p)
             date = p.split('/')
             year = date[2]
             mon = date[3]
@@ -35,40 +40,38 @@ class ScrapTop500:
                 print(urlAux)
                 response = requests.get(urlAux, params=params)
                 soup = BeautifulSoup(response.text, 'html.parser')
-                table = soup.find('table', {'class':"table table-condensed table-striped"})
+                table = soup.find(
+                    'table', {'class': "table table-condensed table-striped"})
                 body = table.find_all('tr')
                 saltar = False
-                lista500Top = []                
-               
+                # lista500Top = []
+
                 for b in body:
                     if not saltar:
                         saltar = True
                         continue
-                  
+
                     b = b.find_all("td")
-                    
+
                     site = b[1].find('a').text
                     pais = b[1].text[len(site):]
                     print(pais)
                     example = {
-                    "rank": b[0].text,
-                    "site": site,
-                    "pais":pais,
-                    "system": b[2].text,
-                    "cores": b[3].text,
-                    "rmax":b[4].text,
-                    "rpeak": b[5].text,
-                    "power":b[6].text,
-                    'año' : year,
-                    'mes' : mon
+                        "rank": b[0].text,
+                        "site": site,
+                        "pais": pais,
+                        "system": b[2].text,
+                        "cores": int(b[3].text.replace(',','')),
+                        "rmax": float(b[4].text.replace(',','')),
+                        "rpeak": float(b[5].text.replace(',','')),
+                        "power": int(b[6].text.replace(',','')) if (b[6].text != '') else None,
+                        'año': int(year),
+                        'mes': int(mon)
                     }
-                    lista500Top.append(example)    
-                    print(lista500Top)
-                   
-                    
-                    
-                
-                
-scrap = ScrapTop500()
+                    self.db.insert(example)
+                    #lista500Top.append(example)
 
-scrap.getTables()
+
+if __name__ == '__main__':
+    scrap = ScrapTop500()
+    scrap.getTables()
